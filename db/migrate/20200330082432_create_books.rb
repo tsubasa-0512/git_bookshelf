6c20b0f9701d3d614f9400cc844893191d6d67e3
EC2 Default User <ec2-user@ip-172-31-32-66.ap-northeast-1.compute.internal>
2020-03-30 08:29:48 +0000

    bookDBをmigrate

--- db/migrate/20200330082432_create_books.rb
+++ db/migrate/20200330082432_create_books.rb
@@ -1,7 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
-
+      t.text :title
+      t.text :author
+      t.integer :genre_id
+      t.integer :review_id
       t.timestamps
     end
   end
