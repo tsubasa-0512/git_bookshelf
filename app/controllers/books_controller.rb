2f08a035d8b51b649b751b518d2734a3f813c3fe
EC2 Default User <ec2-user@ip-172-31-32-66.ap-northeast-1.compute.internal>
2020-04-03 11:24:09 +0000

    new.createアクション.view作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,14 @@ class BooksController < ApplicationController
     def new
         @book = Book.new
     end
+    
+    def create
+        Book.create(create_params)
+        redirect_to :root and return
+    end
+    
+    private
+    def create_params
+        params.require(:book).permit(:title, :genre_id, :author) 
+    end
 end
