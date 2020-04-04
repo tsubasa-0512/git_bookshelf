cdcac9aff964916c761fc4dc96805849f7a3b5d6
EC2 Default User <ec2-user@ip-172-31-32-66.ap-northeast-1.compute.internal>
2020-04-04 09:07:17 +0000

    destroy,searchアクション、html、css作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ApplicationRecord
     belongs_to :genre
+    
+    def self.search(search)
+        if search
+            where(['title LIKE(?)',"%#{search}%"])
+        else
+            all
+        end
+    end
 end
